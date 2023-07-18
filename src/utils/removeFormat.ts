25736925ec61870d1792e23a8f47db9cc9d7f80f
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-07-18 17:58:40 +0200

    fix: move files to another folder

---
+++ src/utils/removeFormat.ts
@@ -0,0 +1,53 @@
+import _ from 'lodash'
+import assign from 'lodash.assign'
+
+import type { instance, value } from '../index'
+import type { customObjectValue } from '../transformationSteps/4-transform'
+
+/**
+ * Remove the Coiote format from custom object
+ */
+export const removeFormat = (
+	input: instance,
+): customObjectValue | customObjectValue[] | undefined => {
+	const data = dataCleaning(input)
+	const result = buildStruct(data)
+
+	return result.length === 1 ? result[0] : result
+}
+
+/**
+ * Remove the 'value' key from element
+ * and remove empty elements
+ */
+const dataCleaning = (rawData: instance): customObjectValue[][] => {
+	const cleanData = Object.values(rawData).map((object) => {
+		const attributesList = Object.entries(object)
+		return attributesList
+			.map((element) => {
+				const id = element[0]
+				const value = element[1]
+
+				// empty value
+				if (Object.keys(value).length === 0) return undefined
+
+				const newValue = value as value
+
+				return {
+					[id]: newValue.value,
+				}
+			})
+			.filter((result) => result !== undefined)
+	})
+	return cleanData as customObjectValue[][]
+}
+
+/**
+ * Build expected struct
+ */
+const buildStruct = (input: customObjectValue[][]) =>
+	input.reduce((previus, current) => {
+		const result = assign.apply(_, current as any)
+		previus.push(result)
+		return previus
+	}, [])
