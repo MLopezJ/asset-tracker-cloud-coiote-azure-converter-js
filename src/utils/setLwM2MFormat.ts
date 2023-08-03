a4718b4b2fe7695224c5bdd71d3006644e8f75dd
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-08-03 10:33:05 +0200

    fix: set LwM2M format individualy
    Update input and output of the method in order to make it possible to set the LwM2M format individually to each object

--- src/utils/setLwM2MFormat.ts
+++ src/utils/setLwM2MFormat.ts
@@ -7,30 +7,27 @@ import { convertToLwM2MInstance } from './convertToLwM2MInstance'
 /**
  * Set LwM2M format using @nordicsemiconductor/lwm2m-types json schema
  */
-export const setLwM2MFormat = (objects: objectWithUrn[]): LwM2MDocument =>
-	objects.reduce((previousObjects, current) => {
-		const urn = Object.keys(current)[0]
-		const instances = Object.values(current)[0]
+export const setLwM2MFormat = (object: objectWithUrn): LwM2MDocument => {
+	const urn = Object.keys(object)[0]
+	const instances = Object.values(object)[0]
 
-		if (urn === undefined || instances === undefined) {
-			console.error('missing values ', { urn, instances })
-			return {}
-		}
+	if (urn === undefined || instances === undefined) {
+		console.error('missing values ', { urn, instances })
+		return {}
+	}
 
-		const schema =
-			LwM2MDocumentSchema.properties[
-				urn as unknown as keyof (typeof LwM2MDocumentSchema)['properties']
-			]
+	const schema =
+		LwM2MDocumentSchema.properties[
+			urn as unknown as keyof (typeof LwM2MDocumentSchema)['properties']
+		]
 
-		if (schema.type === 'array') {
-			return {
-				[urn]: convertToLwM2MArrayInstance(instances, schema),
-				...previousObjects,
-			}
-		}
-
+	if (schema.type === 'array') {
 		return {
-			[urn]: convertToLwM2MInstance(instances, schema),
-			...previousObjects,
-		} // instances should be instance, because it is an object in this case
-	}, {}) as unknown as LwM2MDocument
+			[urn]: convertToLwM2MArrayInstance(instances, schema),
+		}
+	}
+
+	return {
+		[urn]: convertToLwM2MInstance(instances, schema),
+	} // instances should be instance, because it is an object in this case
+}
